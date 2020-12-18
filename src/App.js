fd69c7df7313bef7008dfa21f0450b5bd0fd15b8
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-18 14:24:44 +0900

    object-and-useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
 
-const App = props => {
+const App = (props) => {
 
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  const [state, setState] = useState(props);
+  const {name,price}=state;
 
   return (
     <>
-    <button onClick={()=>setPrice(price+1)}>+1</button>
-    <button onClick={()=>setPrice(price-1)}>-1</button>
-    <button onClick={()=>setPrice(props.price)}>clear</button>
-    <br/>
-    <input value={name} onChange={(e)=>setName(e.target.value)}></input>
-    <button onClick={()=>setName(props.name)}>clear</button>
-    <p>現在の{name}は{price}です</p>
+      <p>現在の{name}は{price}です</p>
+      <button onClick={() => setState({...state,price:price + 1})}>+1</button>
+      <button onClick={() => setState({...state,price:price - 1})}>-1</button>
+      <br />
+      <input value={state.name} onChange={(e) => setState({...state,name:e.target.value})}></input>
+      <button onClick={() => setState(props)}>clear</button>
     </>
   );
 }
 
 //propsの初期値
 App.defaultProps = {
-  name:'sample',
-  price:1000
+  name: 'sample',
+  price: 1000
 }
 
 export default App;
