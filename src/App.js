3b13fb1e0f1d098700f28f26b67afaa7b4edf902
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-18 13:40:03 +0900

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App =() =>{
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const countUp = () => {
+    setCount((prev) =>prev + 1);
+
+  }
+  const countDown = () => {
+    setCount((prev) =>prev - 1)
+  }
+  const countUp2 = () => {
+    setCount((prev) =>prev + 2);
+
+  }
+  const countDown2 = () => 
+    setCount((prev) => rev - 2)
+  }
+
+  const double = () => {
+    setCount((prev) => prev * 2)
+  }
+
+  const divide3 = () => {
+    setCount((prev) => prev % 3 === 0 ? prev / 3 : prev)
+  }
+
+  const reset = () => {
+    setCount(() => {
+      setCount(0);
+    })
+  }
+
+
   return (
-    <div>
-      this is a template for React app
-    </div>
+    <>
+      <div>const:{count}</div>
+      <button onClick={countUp}>+1</button>
+      <button onClick={countDown}>-1</button>
+      <br />
+      <button onClick={countUp2}>+2</button>
+      <button onClick={countDown2}>-2</button>
+      <br />
+      <button onClick={double}>x2</button>
+      <br />
+      <button onClick={divide3}>3で割り切れる時3で割る</button>
+      <br />
+      <button onClick={reset}>clear</button>
+    </>
   );
 }
 
