24db67218761b78217de3d88d959aebfcf876134
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-18 14:48:02 +0900

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = (props) => {
 
   const [state, setState] = useState(props);
-  const {name,price}=state;
+  const { name, price } = state;
+
+  useEffect(() => {
+    console.log("componentDidmount componentDidupdate")
+  });
+
+  useEffect(() => {
+    console.log("name changed")
+  }, [name]);
 
   return (
     <>
       <p>現在の{name}は{price}です</p>
-      <button onClick={() => setState({...state,price:price + 1})}>+1</button>
-      <button onClick={() => setState({...state,price:price - 1})}>-1</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
       <br />
-      <input value={state.name} onChange={(e) => setState({...state,name:e.target.value})}></input>
+      <input value={state.name} onChange={(e) => setState({ ...state, name: e.target.value })}></input>
       <button onClick={() => setState(props)}>clear</button>
     </>
   );
