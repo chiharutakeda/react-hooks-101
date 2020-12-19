99edaa5b3f5dc55bb0b187d9b5d1e3fb942815bb
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-19 13:40:41 +0900

    refactoring with useContext

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,13 @@ import AppContext from "../contexts/AppContext";
 
 import reducer from '../reducers'
 
-console.log(AppContext)
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
   return (
-    <AppContext.Provider value={'hello i am a provider'}>
+    <AppContext.Provider value={{state,dispatch}}>
       <div className='container-fluid'>
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
